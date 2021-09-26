96f3c9da16e37c0dcad243805273dfa1cc6362c0
iskan.mustamir <iskan.mustamir@365.ugm.ac.id>
2021-09-26 13:59:25 +0700

    creating folders

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const homeRoute = require("./routes/home");
+
 const app = express();
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//Middleware
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+//Routing
+app.use("/", homeRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
