4b666f4b0581dd23d9e645618970e3c2d57c3306
iskan.mustamir <iskan.mustamir@365.ugm.ac.id>
2021-09-26 14:39:10 +0700

    Connecting to mongoose db

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const homeRoute = require("./routes/home");
 
 const app = express();
 
 const port = 3000;
+mongoose.connect("mongodb://localhost:27017/nodejs_crud");
+
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("Something went wrong when connecting to the database");
+});
+db.once("open", () => {
+  console.log("Database connected successfully");
+});
 
 //Middleware
 app.set("view engine", "ejs");
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+// router.get("/", (req, res, next) => {
+//   res.send("Express router is working");
+// });
+
 router.get("/", (req, res, next) => {
-  res.send("Express router is working");
+  res.render("home");
 });
 
 module.exports = router;
