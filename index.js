25d46b4c038900b9901cff668fad5db8a6b29bca
iskan.mustamir <iskan.mustamir@365.ugm.ac.id>
2021-09-26 15:53:14 +0700

    Styling and Connecting db to page

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const keys = require("./config/key");
+const perusahaan = require("./models/Perusahaan");
+
 const homeRoute = require("./routes/home");
 
 const app = express();
 
 const port = 3000;
-mongoose.connect("mongodb://localhost:27017/nodejs_crud");
+mongoose.connect(keys.mongoURI);
 
 const db = mongoose.connection;
-db.on("error", () => {
-  console.log("Something went wrong when connecting to the database");
+db.on("error", (err) => {
+  console.log("Error when connecting to MongoDB:", err);
 });
 db.once("open", () => {
-  console.log("Database connected successfully");
+  console.log("MongoDB connected successfully");
 });
 
 //Middleware
+// viewengine setup
 app.set("view engine", "ejs");
+//static folder root setup
 app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 //Routing
 app.use("/", homeRoute);
--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const perusahaan = require("../models/Perusahaan");
 
-// router.get("/", (req, res, next) => {
-//   res.send("Express router is working");
-// });
-
+// Route to read
 router.get("/", (req, res, next) => {
-  res.render("home");
+  perusahaan
+    .find((err, docs) => {
+      res.render("home", { perusahaans: docs });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// Route to add data
+router.post("/add", (req, res, next) => {
+  //Requesting datas from page
+  const [nama, umur, pegawai] = req.body;
+
+  console.log(nama, umur, pegawai);
+
+  // Saving datas to mongoDB
+  const Perusahaan = new perusahaan({
+    nama,
+    umur,
+    pegawai,
+  });
+
+  Perusahaan.save((err) => {
+    if (err) console.log("Something went wrong", err);
+    else {
+      console.log("Data saved successfully");
+      res.redirect("/");
+    }
+  });
 });
 
 module.exports = router;
