4dd23f2e31eacf96ae7d1d0772aaabd68ab7a9e7
DhiasNaufal <dhiasmnaufal0@mail.ugm.ac.id>
2021-09-27 23:07:52 +0700

    menambah halaman Add user

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
 app.get('/', (req, res) => {
   res.render("index")
 })
+app.get('/add-user', (req, res) => {
+    res.render("add_user")
+  })
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
