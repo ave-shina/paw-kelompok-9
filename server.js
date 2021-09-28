60a4b075c558c3bacf84f4df94a0b2e038694eea
Rahma Maesarah <rahma.m@mail.ugm.ac.id>
2021-09-28 15:26:03 +0700

    fix error

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const morgan = require('morgan');
 const bodyparser = require('body-parser');
 const path = require("path")
 
-const connectDB = require('./database/connection');
+const connectDB = require('./server/database/connection');
 
 const app = express()
 
-dotenv.config({path:'config.env'})
-const PORT = process.env.PORT || 8080
+const PORT = 3000;
 
 //log request
 app.use(morgan('tiny'));
 
 //mongodb connection
-connectDB();
+
 
 //parse request to body-parser
 app.use(bodyparser.urlencoded({extended:true}));
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,7 +2,6 @@ const express = require('express');
 const route = express.Router()
 
 const services = require('../services/render');
-const controller = require('../controller/controller');
 
 /**
  * @description Root Route
@@ -21,11 +20,5 @@ route.get('/add-user', services.add_user);
  * @method GET /update-user
  */
 route.get('/update-user', services.update_user);
-
-//API
-route.post('/api/users',controller.create); 
-route.get('/api/users',controller.find);
-route.put('/api/users/:id',controller.update);
-route.delete('/api/users/:id',controller.delete);
 
 module.exports = route;
