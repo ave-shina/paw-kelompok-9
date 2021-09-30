995c3ab79e5fe506a0b42695cd321062d7551d05
iskan.mustamir <iskan.mustamir@365.ugm.ac.id>
2021-09-30 20:15:09 +0700

    fixing some update bug

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -44,18 +44,25 @@ exports.add = async (req, res) => {
 
 //update a data
 exports.update = async (req, res) => {
-  try {
-    const updatedPerusahaan = await perusahaan.findOneAndUpdate(
+  await perusahaan
+    .findByIdAndUpdate(
       {
         _id: req.params.id,
       },
-      req.body,
-      { new: true }
-    );
-    res.json(updatedPerusahaan);
-  } catch (err) {
-    res.json({ message: err });
-  }
+      req.body
+    )
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot Update user with ${req.params.id}. Maybe user not found!`,
+        });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error Update user information" });
+    });
 };
 
 //delete a data
