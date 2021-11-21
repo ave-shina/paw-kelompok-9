2766c25bd48191e0fa8ef2bdbe6ce83d844d5118
iskan.mustamir <iskan.mustamir@365.ugm.ac.id>
2021-11-21 10:57:12 +0700

    feat(API): Add API to client

---
+++ client/api/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const url = "http://localhost:3000/api/employees";
+
+export const fetchEmployee = () => axios.get(url);
+export const createEmployee = (newEmployee) => axios.post(url, newEmployee);
+export const deleteEmployee = (id) => axios.delete(`${url}/:id=${id}`);
+export const updateEmployee = (id, updateEmployee) =>
+  axios.put(`${url}/:id=${id}`, updateEmployee);
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+// import Navbar from "./components/Navbar";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
 
-function App() {
+import { useDispatch } from "react-redux";
+// import { getAgendas } from "./actions/agendas";
+
+export default function App() {
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(getAgendas());
+  // }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <Navbar /> */}
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/edit" exact component={Edit} />
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
